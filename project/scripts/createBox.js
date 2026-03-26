12c0580c89ee85f260c4d4f66461e64e7592cb8a
20RB08 <r.bauer@students.htl-leonding.ac.at>
2026-03-26 10:42:23 +0100

    Footer und Nav in js ausgelet

---
+++ project/scripts/createBox.js
@@ -0,0 +1,58 @@
+/*********************************
+ * Create Footer
+ * ******************************/
+const footer = document.getElementById('footer');
+const footerInput = `<hr class="seperate-s">
+
+                <!--nav-->
+                <div id="footer-nav">
+                    <!--Logo-->
+                    <div class="logo">
+                        Snow<span>trickr®</span>
+                    </div>
+
+                    <!--Infos-->
+                    <div id="footer-infos">
+                        <!--info-->
+                        <div class="footer-info">
+                            <div class="fh">About</div>
+                            <div>Contact</div>
+                            <div>Support</div>
+                        </div>
+                        <!--info-->
+                        <div class="footer-info">
+                            <div class="fh">Snowboarding</div>
+                            <div>Contact</div>
+                            <div>Support</div>
+                        </div>
+                    </div>
+                </div>`
+function createFooter() {
+    footer.innerHTML = footerInput;
+}
+createFooter();
+
+/*********************************
+ * Create Nav
+ * ******************************/
+const nav = document.getElementById('nav');
+const navInput = `<!--left-->
+        <div id="nav-left">
+            <!--Logo-->
+            <div class="logo">
+                Snow<span>trickr®</span>
+            </div>
+            <!--wege-->
+            <a href="index.html" id="aktivePage">Home</a>
+            <a href="#">Browse Tricks</a>
+        </div>
+
+        <!--left-->
+        <div id="nav-right">
+            <!--Login-->
+            <a href="" class="button">Sign up</a>
+        </div>`;
+function createNav() {
+    nav.innerHTML = navInput;
+}
+createNav();
